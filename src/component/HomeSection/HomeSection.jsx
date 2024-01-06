346857713c1daf23ae4dfbfb3c0a31849e702c12
unknown <adlaneould@gmail.com>
2024-01-06 20:00:39 +0100

    c

--- src/component/HomeSection/HomeSection.jsx
+++ src/component/HomeSection/HomeSection.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
-import './../HomeSection/HomeSection.css'
-import Header from '../Header/Header.jsx'
+import "./../HomeSection/HomeSection.css";
+import Header from "../Header/Header.jsx";
+import web from "../../media/Web-alone.png";
+import mobile from "../../media/mobile-alone.png";
+import webAndMobile from "../../media/WebAndMobile.png";
 
 class HomeSection extends Component {
   render() {
     return (
-      <div>
+      <div className="home-section-master">
         <div class="home-section noise">
-            <Header/>
+          <Header />
           <div class="home-content">
-            <h1>Adlane Ould mohand The Best Dev for the Best Future !</h1>
-            <h2>Portfolio</h2>
+            <h1 className="home-title">Adlane Ould Mohand</h1>
+            <h1 className="home-title">A . M</h1>
+            <h2 className="home-sub-title">Developpeur</h2>
+            <div className="web-and-mobile">
+               <div className="title-and-image-container">
+                <h2>Web</h2>
+                <div className="image-container">
+                  <img src={web} alt="" srcset="" />
+                </div>
+              </div>
+              <div className="title-and-image-container">
+                <h2>mobile</h2>
+                <div className="image-container">
+                  <img src={mobile} alt="" srcset="" />
+                </div>
+              </div>  
+            </div>
           </div>
         </div>
       </div>
