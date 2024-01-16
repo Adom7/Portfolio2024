ed15add9b9d7a55c0d9197019efe65450d71f188
Adlane Ould Mohand <am@MacBook-AM.local>
2024-01-16 16:03:04 +0100

    clean

--- src/component/HeroSection/HeroSection.jsx
+++ src/component/HeroSection/HeroSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./HeroSection.css";
+
+
 import clay1 from "../../media/floating/Clay_Black0001.png";
 import clay2 from "../../media/floating/Clay_Black0002.png";
 import clay3 from "../../media/floating/Clay_Black0003.png";
@@ -9,39 +11,45 @@ import clay6 from "../../media/floating/Clay_Black0006.png";
 import clay7 from "../../media/floating/Clay_Black0007.png";
 import clay8 from "../../media/floating/Clay_Black0008.png";
 import clay9 from "../../media/floating/Clay_Black0009.png";
-import cla10 from "../../media/floating/Clay_Black0010.png";
+import clay10 from "../../media/floating/Clay_Black0010.png";
 
 const HeroSection = ({
+  sectionName,
   title,
   subtitle,
   description,
   hasButton,
   buttonLabel,
   illustration,
-  socialMedia
+  socialMedia,
+  projectCard,
+  projectCardImage
+
 }) => {
 
+  const cleanSectionName = sectionName.replace(/\s+/g, '')
 
- function getrandomMeteoritePlace(){
-  const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-  return {
-    position: "absolute ",
-    left: Math.round(Math.random() * 80) + "%",
-    marginTop: Math.round(Math.random() * plusOrMinus * 10) + "px",
-    height: "70%",
-  };
+  function getrandomMeteoritePlace() {
+    const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+    return {
+      position: "absolute ",
+      left: Math.round(Math.random() * 80) + "%",
+      marginTop: Math.round(Math.random() * plusOrMinus * 10) + "px",
+      height: "70%",
+    };
   }
 
-  
 
   function getrandomPosition() {
-     return { position: "absolute ",
-      marginRight:  Math.round(Math.random() * 80) + "%",
-      marginBottom:  Math.round(Math.random() * 50) + "%",
-      height: "20%",}
+    return {
+      position: "absolute ",
+      marginRight: Math.round(Math.random() * 80) + "%",
+      marginBottom: Math.round(Math.random() * 50) + "%",
+      height: "20%",
+    }
   }
 
-  const deplacementCSS = ["flight-throught-tl-br", "rotate" , ];
+  const deplacementCSS = ["flight-throught-tl-br", "rotate",];
 
   const clayImages = [
     clay1,
@@ -53,21 +61,20 @@ const HeroSection = ({
     clay7,
     clay8,
     clay9,
-    cla10,
+    clay10,
   ];
 
   const randomDeplacementIndex = Math.floor(Math.random() * deplacementCSS.length);
   const randomDeplacementCSS = deplacementCSS[randomDeplacementIndex];
 
-  function getRandomClayImage(){
-  const randomClayIndex = Math.floor(Math.random() * clayImages.length);
-  const randomClayImage = clayImages[randomClayIndex];
+  function getRandomClayImage() {
+    const randomClayIndex = Math.floor(Math.random() * clayImages.length);
+    const randomClayImage = clayImages[randomClayIndex];
     return randomClayImage
   }
 
-  const imageUrl = illustration ? require(`../../media/${illustration}`) : undefined;
 
-  function getImagePath(image){
+  function getImagePath(image) {
     return require(`../../media/${image}`)
   }
 
@@ -80,6 +87,7 @@ const HeroSection = ({
 
   return (
     <div className="hero-section">
+      <div>
         <img
           className={`${randomDeplacementCSS}`}
           src={getRandomClayImage()}
@@ -87,34 +95,41 @@ const HeroSection = ({
           style={getrandomPosition()}
         />
 
-      <img
-        className="floating-rotate"
-        src={getRandomClayImage()}
-        alt=""
-        style={getrandomMeteoritePlace()}
-      />
+        <img
+          className="floating-rotate"
+          src={getRandomClayImage()}
+          alt=""
+          style={getrandomMeteoritePlace()}
+        />
+      </div>
 
       <div className="hero-card">
         <div className="hero-content">
           <h1 className="home-title">{title}</h1>
           <h2>{subtitle}</h2>
-          <p>{descriptionParagraph}</p>
+          {projectCard ? (
+            <div className="project-card">
+              <img src={getImagePath(projectCardImage)} alt="" />
+            </div>
+          ) : (
+            <p>{descriptionParagraph}</p>
+          )}
           {socialMedia && (
-          <div className="hero-social-media-icons">
-            {socialMedia.map((media, index) => (
-              <a key={index} href={media.socialLink} target="_blank" rel="noopener noreferrer">
-                <img src={getImagePath(media.imageLink)} alt={`Social media ${index + 1}`} />
-              </a>
-            ))}
-          </div>
-        )}
+            <div className="hero-social-media-icons">
+              {socialMedia.map((media, index) => (
+                <a key={index} href={media.socialLink} target="_blank" rel="noopener noreferrer">
+                  <img src={getImagePath(media.imageLink)} alt={`Social media ${index + 1}`} />
+                </a>
+              ))}
+            </div>
+          )}
           {hasButton && <button className="hero-cta">{buttonLabel}</button>}
         </div>
         {illustration && (
           <img
             src={getImagePath(illustration)}
             alt="illustration"
-            className="hero-card-illustration floating"
+            className={`hero-card-illustration floating ${cleanSectionName} `}
           />
         )}
       </div>
@@ -123,3 +138,4 @@ const HeroSection = ({
 };
 
 export default HeroSection;
+
