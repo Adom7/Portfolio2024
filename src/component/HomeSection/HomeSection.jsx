d75b555b3f86017828007a5c6ef07b765e3e50ff
unknown <adlaneould@gmail.com>
2024-01-25 18:14:30 +0100

    adding HomeSection Config

--- src/component/HomeSection/HomeSection.jsx
+++ src/component/HomeSection/HomeSection.jsx
@@ -7,6 +7,8 @@ import LogoSVG from "../Logos/Logo.jsx";
 
 import heroConfig from "../HeroSection/HeroConfig.json";
 
+const homeSectionConfig = { name : "Adlane Ould Mohand" , pseudo : "A . M" , subTitle1 : "Developpeur" , subTitle2 : "Front End" , presLeft : "Web" , presRight : "Mobile"  }
+
 class HomeSection extends Component {
   render() {
     return (
@@ -14,13 +16,13 @@ class HomeSection extends Component {
         <div className="home-section noise">
           <Header sections={heroConfig} />
           <div class="home-content">
-            <h1 className="home-title">Adlane Ould Mohand</h1>
-            <h1 className="home-title">A . M</h1>
-            <h2 className="home-sub-title">Developpeur </h2>
-            <h2 className="home-sub-title">Front End</h2>
+            <h1 className="home-title">{homeSectionConfig.name}</h1>
+            <h1 className="home-title">{homeSectionConfig.pseudo}</h1>
+            <h2 className="home-sub-title">{homeSectionConfig.subTitle1}</h2>
+            <h2 className="home-sub-title">{homeSectionConfig.subTitle2}</h2>
             <div className="web-and-mobile">
               <div className="title-and-image-container web">
-                <h2>Web</h2>
+                <h2>{homeSectionConfig.presLeft}</h2>
                 <a href="#Prices-Web">
                   <div className="home-illustration">
                     <img
@@ -34,7 +36,7 @@ class HomeSection extends Component {
                 </a>
               </div>
               <div className="title-and-image-container mobile">
-                <h2>Mobile</h2>
+                <h2>{homeSectionConfig.presRight}</h2>
                 <a href="#Prices-Mobile">
                   <div className="home-illustration">
                     <img
